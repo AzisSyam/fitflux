6bacc2e15d9398b9727aab8d76dc08cbbd06feb6
AzisSyam <abdazissyam99@gmail.com>
2023-06-30 20:39:46 +0800

    Perbaiki layout halaman pembuka latihan

--- src/component/pages/gerakan_perut_pemula/PerutPemula.js
+++ src/component/pages/gerakan_perut_pemula/PerutPemula.js
@@ -169,7 +169,7 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 75,
     position: "absolute",
-    bottom: 10,
+    bottom: 20,
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
